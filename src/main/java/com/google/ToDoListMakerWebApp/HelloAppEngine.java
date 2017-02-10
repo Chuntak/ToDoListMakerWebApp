414b27993dd11fc9e596fcdc813c6eb363a92929
Lin <susan.lin@stonybrook.edu>
2017-02-09 22:40:12 -0500

    Login stuffs

--- src/main/java/com/google/ToDoListMakerWebApp/HelloAppEngine.java
+++ src/main/java/com/google/ToDoListMakerWebApp/HelloAppEngine.java
@@ -17,19 +17,31 @@
 package com.google.ToDoListMakerWebApp;
 
 import java.io.IOException;
-
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class HelloAppEngine extends HttpServlet {
-
   @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) 
-      throws IOException {
-      
-    response.setContentType("text/plain");
-    response.getWriter().println("Hello Red Robbins!");
+  public void doGet(HttpServletRequest req, HttpServletResponse resp)
+          throws IOException {
+    UserService userService = UserServiceFactory.getUserService();
 
+    String thisUrl = req.getRequestURI();
+
+    resp.setContentType("text/html");
+    if (req.getUserPrincipal() != null) {
+      resp.getWriter().println("<p>Hello, "
+              + req.getUserPrincipal().getName()
+              + "!  You can <a href=\""
+              + userService.createLogoutURL(thisUrl)
+              + "\">sign out</a>.</p>");
+    } else {
+      resp.getWriter().println("<p>Please <a href=\""
+              + userService.createLoginURL(thisUrl)
+              + "\">sign in</a>.</p>");
+    }
   }
 }
